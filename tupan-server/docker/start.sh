88e54b7680ee63e9f1a7233e56ba6e227f5c1ae2
Gunisalvo <gunisalvo@gmail.com>
2015-06-13 10:41:17 -0300

    jmx

--- tupan-server/docker/start.sh
+++ tupan-server/docker/start.sh
@@ -2,6 +2,8 @@
 /etc/init.d/mongod start &
 java -Dcom.sun.management.jmxremote \
   -Dcom.sun.management.jmxremote.port=9010 \
+  -Dcom.sun.management.jmxremote.rmi.port=9010 \
+  -Djava.rmi.server.hostname='45.55.234.174' \
   -Dcom.sun.management.jmxremote.local.only=false \
   -Dcom.sun.management.jmxremote.authenticate=false \
   -Dcom.sun.management.jmxremote.ssl=false \
